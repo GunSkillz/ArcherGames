a0a7e98f00541ec023dd58944e3b5781a8747ffb
AgentKid <agentkid20@gmail.com>
2012-10-09 21:24:53 -0400

    Fixed some overwritten changes.

--- src/main/java/com/araeosia/ArcherGames/ServerWide.java
+++ src/main/java/com/araeosia/ArcherGames/ServerWide.java
@@ -1,4 +1,3 @@
-
 package com.araeosia.ArcherGames;
 
 import com.araeosia.ArcherGames.utils.Archer;
@@ -10,47 +9,50 @@ import org.bukkit.entity.Player;
  * @author Bruce
  */
 public class ServerWide {
-	
+
 	public ArcherGames plugin;
-	public ArrayList<Player> livingPlayers;
-	public ArrayList<Player> deadPlayers;
-	
-	public ServerWide(ArcherGames plugin){
+	public ArrayList<String> livingPlayers;
+	public ArrayList<String> deadPlayers;
+
+	public ServerWide(ArcherGames plugin) {
 		this.plugin = plugin;
 	}
-	
-	public void killPlayer(Player player){
-		livingPlayers.remove(player);
-		deadPlayers.add(player);
+
+	public void killPlayer(Player player) {
+		livingPlayers.remove(player.getName());
+		deadPlayers.add(player.getName());
 	}
-	public ArrayList<Archer> getNotReadyPlayers(){
-	ArrayList<Archer> archers = new ArrayList<Archer>();
-	   for(Archer a : ArcherGames.players){
-		if(!a.isReady()){
-		    archers.add(a);
+
+	public ArrayList<Archer> getNotReadyPlayers() {
+		ArrayList<Archer> archers = new ArrayList<Archer>();
+		for (Archer a : ArcherGames.players) {
+			if (!a.isReady()) {
+				archers.add(a);
+			}
 		}
-	    }
 		return archers;
 	}
-	public void sendToLivingPlayers(String message){
+
+	public void sendToLivingPlayers(String message) {
 		// Send a message to every living archer
-		for(Player p : livingPlayers){
-			p.sendMessage(message);
+		for (String pName : livingPlayers) {
+			plugin.getServer().getPlayer(pName).sendMessage(message);
 		}
 	}
-	public void sendMessageToAllPlayers(String message){
+
+	public void sendMessageToAllPlayers(String message) {
 		// Send a message to every online player
-		for(Player p : plugin.getServer().getOnlinePlayers()){
+		for (Player p : plugin.getServer().getOnlinePlayers()) {
 			p.sendMessage(message);
 		}
 	}
-	
-	public Player getPlayer(Archer a){
-	    for(Player p : plugin.getServer().getOnlinePlayers()){
-		if(p.getName().equalsIgnoreCase(a.getName())){
-		    return p;
+
+	public Player getPlayer(Archer a) {
+		for (Player p : plugin.getServer().getOnlinePlayers()) {
+			if (p.getName().equalsIgnoreCase(a.getName())) {
+				return p;
+			}
 		}
-	    }
-	    return null;
+		return null;
 	}
 }
