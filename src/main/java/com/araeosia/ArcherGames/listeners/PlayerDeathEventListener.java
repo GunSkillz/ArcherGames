b1af6c320f91d44b20f321355dcaccd0085bf699
AgentKid <agentkid20@gmail.com>
2012-10-09 22:42:41 -0400

    Handles the player death event now.

--- src/main/java/com/araeosia/ArcherGames/listeners/PlayerDeathEventListener.java
+++ src/main/java/com/araeosia/ArcherGames/listeners/PlayerDeathEventListener.java
@@ -1,6 +1,7 @@
 package com.araeosia.ArcherGames.listeners;
 
 import com.araeosia.ArcherGames.ArcherGames;
+import com.araeosia.ArcherGames.utils.Archer;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +22,8 @@ public class PlayerDeathEventListener implements Listener{
 	 */
 	@EventHandler
 	public void onDeathEvent(final PlayerDeathEvent event){
-		
+		// Player has died.
+		plugin.serverwide.killPlayer(event.getEntity());
+		Archer.getByName(event.getEntity().getName()).kill();
 	}
 }
