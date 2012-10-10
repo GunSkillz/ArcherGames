f8a14ad48be5e7fe61d0e99bb8fa9f7b5bb1356b
AgentKid <agentkid20@gmail.com>
2012-10-09 21:15:52 -0400

    Scheduled tasks finished. Appropriate config fields added.

--- src/main/java/com/araeosia/ArcherGames/utils/Config.java
+++ src/main/java/com/araeosia/ArcherGames/utils/Config.java
@@ -46,6 +46,7 @@ public class Config {
 			plugin.getConfig().set("ArcherGames.timers.preGameCountdown", 120); // 2 minutes for everyone to get in game
 			plugin.getConfig().set("ArcherGames.timers.gameInvincibleCountdown", 60); // 1 minute for everyone to get far enough away from each other
 			plugin.getConfig().set("ArcherGames.timers.gameOvertimeCountdown", 600); // 10 minutes to play before we force the round to end
+			plugin.getConfig().set("ArcherGames.timers.shutdownTimer", 30); // 30 seconds until the server reboots.
 			plugin.getConfig().set("ArcherGames.game.startPosition.world", plugin.getServer().getWorlds().get(0)); // Fetch the default world
 			plugin.getConfig().set("ArcherGames.game.startPosition.x", 0);
 			plugin.getConfig().set("ArcherGames.game.startPosition.y", 64);
@@ -55,6 +56,8 @@ public class Config {
 			plugin.getConfig().set("ArcherGames.strings.starting", "[ArcherGames] The Archer Games have started! You have 1 minute of invincibility to get away from enemies.");
 			plugin.getConfig().set("ArcherGames.strings.invincibilityend", "[ArcherGames] Your minute of invincibility is up! Let the games begin...");
 			plugin.getConfig().set("ArcherGames.strings.overtimestart", "[ArcherGames] Overtime has started! Fight to the death!");
+			plugin.getConfig().set("ArcherGames.strings.gameended", "[ArcherGames] The game is over!");
+			plugin.getConfig().set("ArcherGames.strings.serverclosekick", "The server is rebooting.");
 			plugin.saveConfig();
 		}
 		plugin.voteSites = (java.util.List<String>) plugin.getConfig().getList("ArcherGames.vote.sites");
@@ -62,6 +65,7 @@ public class Config {
 		plugin.scheduler.gameInvincibleCountdown = plugin.getConfig().getInt("ArcherGames.timers.gameInvincibleCountdown");
 		plugin.scheduler.gameOvertimeCountdown = plugin.getConfig().getInt("ArcherGames.timers.gameOvertimeCountdown");
 		plugin.scheduler.minPlayersToStart = plugin.getConfig().getInt("ArcherGames.game.minPlayersToStart");
+		plugin.scheduler.shutdownTimer = plugin.getConfig().getInt("ArcherGames.game.shutdownTimer");
 		plugin.startPosition = new Location(
 			plugin.getServer().getWorld(plugin.getConfig().getString("ArcherGames.game.startPosition.world")),
 			plugin.getConfig().getInt("ArcherGames.game.startPosition.x"),
@@ -72,6 +76,8 @@ public class Config {
 		plugin.strings.put("starting", plugin.getConfig().getString("ArcherGames.strings.starting"));
 		plugin.strings.put("invincibilityend", plugin.getConfig().getString("ArcherGames.strings.invincibilityend"));
 		plugin.strings.put("overtimestart", plugin.getConfig().getString("ArcherGames.strings.overtimestart"));
+		plugin.strings.put("gameended", plugin.getConfig().getString("ArcherGames.strings.gameended"));
+		plugin.strings.put("serverclosekick", plugin.getConfig().getString("ArcherGames.strings.serverclosekick"));
 		loadKits();
 		}
 		
