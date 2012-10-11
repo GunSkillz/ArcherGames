61021c4db772886a7fa5403276d011f54058201f
AgentKid <agentkid20@gmail.com>
2012-10-10 22:30:43 -0400

    I believe in you, Jenkins.

--- src/main/java/com/araeosia/ArcherGames/ArcherGames.java
+++ src/main/java/com/araeosia/ArcherGames/ArcherGames.java
@@ -62,7 +62,7 @@ public class ArcherGames extends JavaPlugin {
 		this.getCommand("archergames").setExecutor(new ArcherGamesCommandExecutor(this));
 
 		log.info("ArcherGames is enabled!"); 
-		log.info("Attempting to hook into plugins!");
+		log.info("Attempting to hook into plugins...");
 	}
 
 	/**
