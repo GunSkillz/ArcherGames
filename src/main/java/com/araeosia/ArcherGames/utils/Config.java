d928a6f2499169e97cfe5484df6745a3cad7f9c5
AgentKid <agentkid20@gmail.com>
2012-10-14 13:58:31 -0400

    Try this.

--- src/main/java/com/araeosia/ArcherGames/utils/Config.java
+++ src/main/java/com/araeosia/ArcherGames/utils/Config.java
@@ -44,14 +44,6 @@ public class Config {
 		plugin.getConfig().set("ArcherGames.timers.gameOvertimeCountdown", plugin.getConfig().get("ArcherGames.timers.gameOvertimeCountdown", 600)); // 10 minutes to play before we force the round to end
 		plugin.getConfig().set("ArcherGames.timers.shutdownTimer", plugin.getConfig().get("ArcherGames.timers.shutdownTimer", 30)); // 30 seconds until the server reboots.
 		plugin.getConfig().set("ArcherGames.timers.nagTime", plugin.getConfig().get("ArcherGames.timers.nagTime", 30));
-		/*
-		 * plugin.getConfig().set("ArcherGames.game.startPosition.world",
-		 * plugin.getServer().getWorlds().get(0).getName()); // Fetch the
-		 * default world
-		 * plugin.getConfig().set("ArcherGames.game.startPosition.x", 0);
-		 * plugin.getConfig().set("ArcherGames.game.startPosition.y", 64);
-		 * plugin.getConfig().set("ArcherGames.game.startPosition.z", 0);
-		 */
 		plugin.getConfig().set("ArcherGames.game.minPlayersToStart", plugin.getConfig().get("ArcherGames.game.minPlayersToStart", 5));
 		plugin.getConfig().set("ArcherGames.game.overtimeWorldRadius", plugin.getConfig().get("ArcherGames.game.overtimeWorldRadius", 50));
 		plugin.getConfig().set("ArcherGames.game.arrowExplosionFactor", plugin.getConfig().get("ArcherGames.game.overtimeWorldRadius", 2));
@@ -76,7 +68,7 @@ public class Config {
 		plugin.getConfig().set("ArcherGames.strings.nochestediting", plugin.getConfig().get("ArcherGames.strings.nochestediting", "§4You cannot access this until the game begins!"));
 		plugin.getConfig().set("ArcherGames.strings.nodroppickup", plugin.getConfig().get("ArcherGames.strings.nodroppickup", "§4You cannot drop or pick up items until the game begins!"));
 		plugin.getConfig().set("ArcherGames.strings.playersleft", plugin.getConfig().get("ArcherGames.strings.playersleft", "§c%s players remaining."));
-		plugin.getConfig().set("ArcherGames.strings.pingreply1", plugin.getConfig().get("ArcherGames.strings.playersleft", "Game starts in %."));
+		plugin.getConfig().set("ArcherGames.strings.pingreply1", plugin.getConfig().get("ArcherGames.strings.playersleft", "Game starts in %s."));
 		plugin.getConfig().set("ArcherGames.strings.pingreply2", plugin.getConfig().get("ArcherGames.strings.playersleft", "Game in progress!"));
 		plugin.getConfig().set("ArcherGames.strings.pingreply3", plugin.getConfig().get("ArcherGames.strings.playersleft", "Game in progress!"));
 		plugin.getConfig().set("ArcherGames.strings.pingreply4", plugin.getConfig().get("ArcherGames.strings.playersleft", "Game is in overtime!"));
