02569b047af13cff01b5a912125b0c668683736e
AgentKid <agentkid20@gmail.com>
2012-10-16 07:21:21 -0400

    Commented out signs.

--- src/main/java/com/araeosia/ArcherGames/listeners/BlockEventListener.java
+++ src/main/java/com/araeosia/ArcherGames/listeners/BlockEventListener.java
@@ -64,7 +64,7 @@ public class BlockEventListener implements Listener {
 		}
 	}
 
-	/*@EventHandler
+	@EventHandler
 	public void onBlockInteract(final PlayerInteractEvent event) {
 		if (event.hasBlock()) {
 			if (plugin.debug) {
@@ -77,67 +77,58 @@ public class BlockEventListener implements Listener {
 				if (ScheduledTasks.gameStatus == 1 && !event.getPlayer().hasPermission("archergames.overrides.invedit")) {
 					event.setCancelled(true);
 				}
+			} else if (event.getClickedBlock().getState() instanceof Sign && false) {
+				/*
+				 * Sign sign = (Sign) event.getClickedBlock().getState(); if
+				 * (sign.getLine(0).equals("§1[Enchant]")) {
+				 * event.setCancelled(true); // Line 2: Any, Line 3:
+				 * Enchantment:Level Line 4: Price double price = new
+				 * Double(sign.getLine(3).substring(1)); if
+				 * (plugin.econ.hasBalance(event.getPlayer().getName(), price))
+				 * { String[] data = sign.getLine(2).split(":"); Enchantment
+				 * enchantment = Enchantment.getByName(data[0]); ItemStack
+				 * itemInHand =
+				 * event.getPlayer().getInventory().getItemInHand(); boolean
+				 * isOkay = true; for (Enchantment e :
+				 * itemInHand.getEnchantments().keySet()) { if
+				 * (e.conflictsWith(enchantment) || e.equals(enchantment)) {
+				 * isOkay = false;
+				 * event.getPlayer().sendMessage(plugin.strings.get("enchantmentconflict"));
+				 * } } if
+				 * (enchantment.canEnchantItem(event.getPlayer().getInventory().getItemInHand())
+				 * && isOkay) { ItemStack newItemStack = itemInHand;
+				 * newItemStack.addEnchantment(enchantment,
+				 * Integer.parseInt(data[1]));
+				 * event.getPlayer().getInventory().setItemInHand(newItemStack);
+				 * } //plugin.econ.takePlayer(event.getPlayer().getName(),
+				 * price); } else {
+				 * event.getPlayer().sendMessage(plugin.strings.get("notenoughmoney"));
+				 * } } else if (sign.getLine(0).equals("§1[Buy]")) {
+				 * event.setCancelled(true); // Line 2: Quantity, Line 3: Item
+				 * name, Line 4: Price double price = new
+				 * Double(sign.getLine(3).substring(1)); //if
+				 * (plugin.econ.hasBalance(event.getPlayer().getName(), price))
+				 * { ItemStack itemToGive; if (sign.getLine(2).contains(":")) {
+				 * String[] data = sign.getLine(2).split(":"); int damage = 0;
+				 * if (data.length == 1) { damage = 0; } else { damage =
+				 * Integer.parseInt(data[1]); } plugin.log.info(data[0]);
+				 * Material mat; mat =
+				 * Material.getMaterial(data[0].toUpperCase());
+				 * plugin.log.info(mat.toString()); itemToGive = new
+				 * ItemStack(mat, Integer.parseInt(data[1])); }else{ Material
+				 * mat; plugin.log.info(sign.getLine(2)); mat =
+				 * Material.getMaterial(sign.getLine(2).toUpperCase());
+				 * plugin.log.info(mat.toString()); itemToGive = new
+				 * ItemStack(mat); }
+				 *
+				 * event.getPlayer().getInventory().addItem(itemToGive);
+				 * //plugin.econ.takePlayer(event.getPlayer().getName(), price);
+				 * } else {
+				 * event.getPlayer().sendMessage(plugin.strings.get("notenoughmoney"));
+				 * } }
 			}
-			if (event.getClickedBlock().getState() instanceof Sign) {
-				Sign sign = (Sign) event.getClickedBlock().getState();
-				if (sign.getLine(0).equals("§1[Enchant]")) {
-					event.setCancelled(true);
-					// Line 2: Any, Line 3: Enchantment:Level Line 4: Price
-					double price = new Double(sign.getLine(3).substring(1));
-					if (plugin.econ.hasBalance(event.getPlayer().getName(), price)) {
-						String[] data = sign.getLine(2).split(":");
-						Enchantment enchantment = Enchantment.getByName(data[0]);
-						ItemStack itemInHand = event.getPlayer().getInventory().getItemInHand();
-						boolean isOkay = true;
-						for (Enchantment e : itemInHand.getEnchantments().keySet()) {
-							if (e.conflictsWith(enchantment) || e.equals(enchantment)) {
-								isOkay = false;
-								event.getPlayer().sendMessage(plugin.strings.get("enchantmentconflict"));
-							}
-						}
-						if (enchantment.canEnchantItem(event.getPlayer().getInventory().getItemInHand()) && isOkay) {
-							ItemStack newItemStack = itemInHand;
-							newItemStack.addEnchantment(enchantment, Integer.parseInt(data[1]));
-							event.getPlayer().getInventory().setItemInHand(newItemStack);
-						}
-						plugin.econ.takePlayer(event.getPlayer().getName(), price);
-					} else {
-						event.getPlayer().sendMessage(plugin.strings.get("notenoughmoney"));
-					}
-				} else if (sign.getLine(0).equals("§1[Buy]")) {
-					event.setCancelled(true);
-					// Line 2: Quantity, Line 3: Item name, Line 4: Price
-					double price = new Double(sign.getLine(3).substring(1));
-					if (plugin.econ.hasBalance(event.getPlayer().getName(), price)) {
-						ItemStack itemToGive;
-						if (sign.getLine(2).contains(":")) {
-							String[] data = sign.getLine(2).split(":");
-							int damage = 0;
-							if (data.length == 1) {
-								damage = 0;
-							} else {
-								damage = Integer.parseInt(data[1]);
-							}
-							plugin.log.info(data[0]);
-							Material mat;
-							mat = Material.getMaterial(data[0].toUpperCase());
-							plugin.log.info(mat.toString());
-							itemToGive = new ItemStack(mat, Integer.parseInt(data[1]));
-						}else{
-							Material mat;
-							plugin.log.info(sign.getLine(2));
-							mat = Material.getMaterial(sign.getLine(2).toUpperCase());
-							plugin.log.info(mat.toString());
-							itemToGive = new ItemStack(mat);
-						}
-
-						event.getPlayer().getInventory().addItem(itemToGive);
-						plugin.econ.takePlayer(event.getPlayer().getName(), price);
-					} else {
-						event.getPlayer().sendMessage(plugin.strings.get("notenoughmoney"));
-					}
-				}
+				 */
 			}
 		}
-	}*/
+	}
 }
