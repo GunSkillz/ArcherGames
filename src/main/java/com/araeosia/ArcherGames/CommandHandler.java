ab55b0f024016f8049ff922b1d69c3895b9f8158
zippy120 <Jason@dhmc.us>
2012-10-18 21:22:12 -0600

    added a permission to the previous commit

--- src/main/java/com/araeosia/ArcherGames/CommandHandler.java
+++ src/main/java/com/araeosia/ArcherGames/CommandHandler.java
@@ -310,32 +310,33 @@ public class CommandHandler implements CommandExecutor, Listener {
 		sendHelp(sender, "goto (player)", "Teleport to anothe rplayer while spectating.");
 			return true;
 		} else if(cmd.getName().equalsIgnoreCase("goto")){
-			if(args.length != 0){
-				Player p = null;
-				for(Player player : plugin.getServer().getOnlinePlayers()){
-					if(player.getName().contains(args[0])){
-						p = player;
+			if(sender.hasPermission("ArcherGames.commands.goto")){
+				if(args.length != 0){
+					Player p = null;
+					for(Player player : plugin.getServer().getOnlinePlayers()){
+						if(player.getName().contains(args[0])){
+							p = player;
+						}
 					}
-				}
-				if(p != null){
-					if(sender instanceof Player){
-						if(!Archer.getByName(sender.getName()).isAlive){
-							((Player) sender).teleport(p);
-							sender.sendMessage(ChatColor.GREEN + "You have been teleported to " + p.getName());
-							return true;
+					if(p != null){
+						if(sender instanceof Player){
+							if(!Archer.getByName(sender.getName()).isAlive){
+								((Player) sender).teleport(p);
+								sender.sendMessage(ChatColor.GREEN + "You have been teleported to " + p.getName());
+								return true;
+							} else {
+								sender.sendMessage("You must be dead to use that command!");
+								return true;
+							}
 						} else {
-							sender.sendMessage("You must be dead to use that command!");
+							sender.sendMessage(ChatColor.RED + "You must be a player to execute that command!");
 							return true;
 						}
 					} else {
-						sender.sendMessage(ChatColor.RED + "You must be a player to execute that command!");
+						sender.sendMessage(ChatColor.RED + "That is not a valid player!");
 						return true;
 					}
-				} else {
-					sender.sendMessage(ChatColor.RED + "That is not a valid player!");
-					return true;
 				}
-				
 			}
 		}
 		return false;
